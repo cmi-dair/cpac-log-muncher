9ff8902c811d8858d95f7fc99c5f6aeb6c693019
Florian Rupprecht <floruppr@gmail.com>
2023-10-27 15:12:55 -0400

    Refactoring

--- src/clmunch/clmunch.py
+++ src/clmunch/clmunch.py
@@ -4,157 +4,14 @@ from datetime import datetime
 import pandas as pd
 import argparse
 import numpy as np
-from typing import Dict, Any
+from typing import Any
 import shlex
-
-ICO_SUCCESS = "&#9989;"
-ICO_FAILURE = "&#10060;"
-
-TEMPLATE_REPORT_MD = """# CPAC run report
-
-{header}
-
-## Summary
-
-{summary}
-
-## Details
-
-{details}
-
-<hr>
-
-{footer}
-
-"""
-
-TEMPLATE_ENTRY_MD = """### {file}
-
-{details}
-
-"""
-
-TEMPLATE_SPOILER_MD = """<details>
-<summary>{summary}</summary>
-
-{details}
-
-</details>
-"""
+from . import utils
 
 
-def crashfile_to_md(crashfile: pl.Path):
-    with open(crashfile, "r") as f:
-        crashfile_content = f.read()
-        return TEMPLATE_SPOILER_MD.format(
-            summary=f"Crashfile <code>{crashfile.name}</code>",
-            details=f"```Python\n{crashfile_content}```",
-        )
+# REGEXES
 
-
-def make_details(stats: Dict[str, Any]):
-    crashfiles_md = "\n".join(
-        [crashfile_to_md(crashfile) for crashfile in stats["crashfiles"]]
-    )
-
-    del stats["crashfiles"]
-
-    success = stats["success"]
-    file = stats["file"]
-
-    stats["success"] = ICO_SUCCESS if success else ICO_FAILURE
-
-    stats["command"] = "<br/>".join(shlex.split(stats["command"]))
-    stats["command"] = f"<code>{stats['command']}</code>"
-
-    # monospace fields
-    for k in ["file", "version"]:
-        stats[k] = f"`{stats[k]}`"
-
-    details_md = TEMPLATE_ENTRY_MD.format(
-        file=stats["pipeline_config"],
-        details=pd.DataFrame(
-            {"key": stats.keys(), "value": stats.values()}
-        ).to_markdown(index=False),
-    )
-
-    if not success:
-        logfile_tail = file_tail(file, 100)
-
-        crashfiles_md += "\n" + TEMPLATE_SPOILER_MD.format(
-            summary="Last 100 lines of logfile",
-            details=f"```log\n{logfile_tail}```",
-        )
-
-    return details_md + crashfiles_md
-
-
-def file_tail(file: pl.Path, n: int = 10):
-    with open(file, "r", encoding="UTF-8") as f:
-        lines = f.readlines()
-        return "".join(lines[-n:])
-
-
-RX_MARKDOWN_HEADING_ID_LEGAL_CHARS = re.compile(r"[^0-9a-zA-Z_-]")
-
-
-def markdown_heading_to_id(heading: str):
-    """Convert a markdown heading to a valid id to link to via (my link)[#id]."""
-    return RX_MARKDOWN_HEADING_ID_LEGAL_CHARS.sub("", heading.lower())
-
-
-def markdown_heading_to_link(heading: str, title: str | None = None):
-    """Convert a markdown heading to a link."""
-    title = heading if title is None else title
-    return f"[{title}](#{markdown_heading_to_id(heading)})"
-
-
-def main():
-    parser = argparse.ArgumentParser(description="Generate a report on CPAC runs.")
-    parser.add_argument(
-        "path", type=str, help="Path to the directory containing the log files."
-    )
-    parser.add_argument(
-        "-o", "--output", type=str, help="Path to the output file.", required=False
-    )
-    args = parser.parse_args()
-
-    path_searchdir = pl.Path(args.path)
-    log_files = find_log_files(path_searchdir)
-    stats = [extract_info(pl.Path(f), path_searchdir) for f in log_files]
-
-    df = pd.DataFrame.from_records(stats)
-
-    df["success_state"] = df["success"]
-    df["success"] = np.where(df["success"], ICO_SUCCESS, ICO_FAILURE)
-
-    df["pipeline_config"] = df["pipeline_config"].apply(
-        lambda x: markdown_heading_to_link(x)
-    )
-
-    details = "\n".join([make_details(x) for x in stats])
-
-    report = TEMPLATE_REPORT_MD.format(
-        header=f"Ran {len(stats)} CPAC pipelines with {df['success_state'].sum() / len(stats) * 100:.2f}% success rate.\n\n"
-        f"Slowest pipeline took {df['duration'].max()} (first until last log message).\n\n"
-        f"Pipelines found in <code>{path_searchdir}</code>.\n\n",
-        footer=f"*Generated on {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}*",
-        summary=df[["pipeline_config", "duration", "success"]].to_markdown(index=False),
-        details=details,
-    )
-
-    if args.output:
-        with open(args.output, "w", encoding="UTF-8") as f:
-            f.write(report)
-    else:
-        print(report)
-
-
-# find files called 'pypeline.log' in subdirectories
-def find_log_files(root: pl.Path):
-    return root.glob("**/pypeline*.log")
-
-
+# Logfile
 RX_TIMESTAMP = re.compile(r"^\d{6}-\d{2}:\d{2}:\d{2},\d{1,3}")
 RX_CPAC_COMMAND = re.compile(r"^\s*Run command: (.*)$")
 RX_CPAC_VERSION = re.compile(r"^\s*C-PAC version: (.*)$")
@@ -169,77 +26,238 @@ RC_CPAC_END_ERROR = re.compile(r"^\s*CPAC run error:\s*$")
 
 RX_CPAC_PIPELINE_CONFIG_COMMAND_FALLBACK = re.compile(r"--preconfig\s*(\S+)")
 
+RX_CPAC_ERROR1_LOOKUP = re.compile(
+    r"LookupError: When trying to connect node block '([^']+)' to workflow '([^']+)' after node block '([^']+)':\s+\[!\] C-PAC says: None of the listed resources are in the resource pool:\s+(.*)$"
+)
+RX_CPAC_ERROR2_LOOKUP = re.compile(
+    r"LookupError: When trying to connect node block '([^']+)' to workflow '([^']+)' after node block '([^']+)':\s+\[!\] C-PAC says: None of the listed resources in the node block being connected exist in the resource pool\.\s+Resources:\s+(.*)$"
+)
 
-def extract_info(log_file: pl.Path, base_dir: pl.Path):
-    min_time = None
-    max_time = None
 
-    cpac_command = None
-    cpac_test_config = None
-    cpac_version = None
-    cpac_pipeline_config = None
-    cpac_subject_workflow = None
+TEMPLATE_REPORT_MD = """# CPAC run report\n
+{header}\n
+## Summary\n
+{summary}\n
+## Details\n
+{details}\n
+<hr>\n
+{footer}\n
+"""
 
-    cpac_success = False
-    cpac_error = False
+TEMPLATE_ENTRY_MD = """### {file}\n
+{details}\n
+"""
 
-    # read line by line
-    with open(log_file, "r", encoding="UTF-8") as f:
-        while line := f.readline():
-            # match with regex
-            if match := re.match(RX_TIMESTAMP, line):
-                # convert to datetime object
-                stamp = datetime.strptime(match.group(), "%y%m%d-%H:%M:%S,%f")
+TEMPLATE_SPOILER_MD = """<details>
+<summary>{summary}</summary>\n
+{details}\n
+</details>
+"""
 
-                if min_time is None or stamp < min_time:
-                    min_time = stamp
-                if max_time is None or stamp > max_time:
-                    max_time = stamp
 
-            elif match := re.match(RX_CPAC_COMMAND, line):
-                cpac_command = match.group(1)
-                cpac_test_config = " test_config " in cpac_command
-            elif match := re.match(RX_CPAC_VERSION, line):
-                cpac_version = match.group(1)
-            elif match := re.match(RX_CPAC_END_PIPELINE_CONFIG, line):
-                cpac_pipeline_config = match.group(1)
-            elif match := re.match(RX_CPAC_END_SUBJECT_WORKFLOW, line):
-                cpac_subject_workflow = match.group(1)
-            elif (match := re.match(RC_CPAC_END_SUCCESS, line)) or (
-                cpac_test_config
-                and (match := re.match(RC_CPAC_END_SUCCESS_TEST_CONFIG, line))
-            ):
-                cpac_success = True
-            elif match := re.match(RC_CPAC_END_ERROR, line):
-                cpac_error = True
+def find_log_files(root: pl.Path):
+    """Find all log files in the given directory recursively."""
+    return root.glob("**/pypeline*.log")
 
-    # calculate difference
-    if max_time is not None and min_time is not None:
-        diff = max_time - min_time
 
-    # fallback to command line argument or filename
-    if cpac_pipeline_config is None and cpac_command is not None:
-        cpac_pipeline_config = (
-            fb.group(1)
-            if (fb := re.search(RX_CPAC_PIPELINE_CONFIG_COMMAND_FALLBACK, cpac_command))
-            else None
+def find_crash_files(log_file: pl.Path):
+    """Find all crash files associated with a given log file."""
+    return log_file.parent.glob("../../crash-*.txt")
+
+
+class CpacRun:
+    def __init__(self, log_file: pl.Path, base_dir: pl.Path) -> None:
+        self.log_file = log_file
+        self.base_dir = base_dir
+
+        min_time = None
+        max_time = None
+
+        self.command: str | None = None
+        self.test_config: str | None = None
+        self.version: str | None = None
+        self.pipeline_config: str | None = None
+        self.subject_workflow: str | None = None
+
+        cpac_success = False
+        cpac_error = False
+
+        # read line by line
+        with open(log_file, "r", encoding="UTF-8") as f:
+            while line := f.readline():
+                # match with regex
+                if match := re.match(RX_TIMESTAMP, line):
+                    # convert to datetime object
+                    stamp = datetime.strptime(match.group(), "%y%m%d-%H:%M:%S,%f")
+
+                    if min_time is None or stamp < min_time:
+                        min_time = stamp
+                    if max_time is None or stamp > max_time:
+                        max_time = stamp
+
+                elif match := re.match(RX_CPAC_COMMAND, line):
+                    self.command = match.group(1)
+                    self.test_config = " test_config " in self.command
+                elif match := re.match(RX_CPAC_VERSION, line):
+                    self.version = match.group(1)
+                elif match := re.match(RX_CPAC_END_PIPELINE_CONFIG, line):
+                    self.pipeline_config = match.group(1)
+                elif match := re.match(RX_CPAC_END_SUBJECT_WORKFLOW, line):
+                    self.subject_workflow = match.group(1)
+                elif (match := re.match(RC_CPAC_END_SUCCESS, line)) or (
+                    self.test_config
+                    and (match := re.match(RC_CPAC_END_SUCCESS_TEST_CONFIG, line))
+                ):
+                    cpac_success = True
+                elif match := re.match(RC_CPAC_END_ERROR, line):
+                    cpac_error = True
+
+        # calculate difference
+        if max_time is not None and min_time is not None:
+            self.diff = max_time - min_time
+        self.start: datetime | None = min_time
+
+        # fallback to command line argument or filename
+        if self.pipeline_config is None and self.command is not None:
+            self.pipeline_config = (
+                fb.group(1)
+                if (
+                    fb := re.search(
+                        RX_CPAC_PIPELINE_CONFIG_COMMAND_FALLBACK, self.command
+                    )
+                )
+                else None
+            )
+        if self.pipeline_config is None:
+            self.pipeline_config = str(log_file.relative_to(base_dir))
+
+        self.crashfiles = list(find_crash_files(log_file))
+
+        self.success: bool = cpac_success and not cpac_error
+
+    def record(self) -> dict[str, Any]:
+        return {
+            "file": self.log_file,
+            "start": self.start,
+            "duration": self.diff,
+            "command": self.command,
+            "version": self.version,
+            "pipeline_config": self.pipeline_config,
+            "subject_workflow": self.subject_workflow,
+            "success": self.success,
+            "crashfiles": self.crashfiles,
+        }
+
+    @classmethod
+    def crashfile_to_md(cls, crashfile: pl.Path):
+        crashfile_content = ""
+        with open(crashfile, "r") as f:
+            crashfile_content = f.read()
+        return TEMPLATE_SPOILER_MD.format(
+            summary=f"Crashfile <code>{crashfile.name}</code>",
+            details=f"```Python\n{crashfile_content}```",
         )
-    if cpac_pipeline_config is None:
-        cpac_pipeline_config = str(log_file.relative_to(base_dir))
 
-    crashfiles = list(log_file.parent.glob("../../crash-*.txt"))
+    def md_report(self) -> str:
+        out_dict = {
+            "File": f"`{self.log_file.absolute()}`",
+            "Start": self.start,
+            "Duration": self.diff,
+            "Command": "<code>" + "<br/>".join(shlex.split(self.command)) + "</code>",
+            "Version": f"`{self.version}`",
+            "Pipeline Config": self.pipeline_config,
+            "Subject Workflow": self.subject_workflow,
+            "Success": utils.bool_to_emoji(self.success),
+        }
 
-    return {
-        "file": log_file,
-        "start": min_time,
-        "duration": diff,
-        "command": cpac_command,
-        "version": cpac_version,
-        "pipeline_config": cpac_pipeline_config,
-        "subject_workflow": cpac_subject_workflow,
-        "success": cpac_success and not cpac_error,
-        "crashfiles": crashfiles,
-    }
+        details_md = TEMPLATE_ENTRY_MD.format(
+            file=self.pipeline_config,
+            details=pd.DataFrame(
+                {"Key": out_dict.keys(), "Value": out_dict.values()}
+            ).to_markdown(index=False),
+        )
+
+        crashfiles_md = "\n".join(
+            [CpacRun.crashfile_to_md(crashfile) for crashfile in self.crashfiles]
+        )
+
+        if not self.success:
+            logfile_tail = utils.file_tail(self.log_file, 100)
+
+            crashfiles_md += "\n" + TEMPLATE_SPOILER_MD.format(
+                summary="Last 100 lines of logfile",
+                details=f"```log\n{logfile_tail}```",
+            )
+
+        return details_md + crashfiles_md
+
+
+class CpacRunCollection:
+    def __init__(self, search_path: pl.Path, base_path: pl.Path):
+        self.search_path = search_path
+        self.base_path = base_path
+        self.runs = [CpacRun(f, base_path) for f in find_log_files(search_path)]
+
+    def report_md(self) -> str:
+        records = [r.record() for r in self.runs]
+
+        df_overview = pd.DataFrame.from_records(records)
+        df_overview["success_state"] = df_overview["success"]
+        df_overview["success"] = np.where(
+            df_overview["success"], utils.HTML_SYMBOL_SUCCESS, utils.HTML_SYMBOL_FAILURE
+        )
+
+        df_overview["pipeline_config"] = df_overview["pipeline_config"].apply(
+            lambda x: utils.markdown_heading_to_link(x)
+        )
+
+        # Overview table
+        md_table_overview = df_overview[
+            ["pipeline_config", "duration", "success"]
+        ].to_markdown(index=False)
+
+        # Intro text
+        md_intro_text = (
+            f"Ran {len(self.runs)} CPAC pipelines with {df_overview['success_state'].sum() / len(self.runs) * 100:.2f}% success rate.\n\n"
+            f"Slowest pipeline took {df_overview['duration'].max()} (first until last log message).\n\n"
+            f"Pipelines found under <code>{self.search_path}</code>.\n\n"
+        )
+
+        # Footer
+        md_footer = f"*Generated on {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}*"
+
+        # Run details
+        md_details = "\n".join([x.md_report() for x in self.runs])
+
+        return TEMPLATE_REPORT_MD.format(
+            header=md_intro_text,
+            footer=md_footer,
+            summary=md_table_overview,
+            details=md_details,
+        )
+
+
+def make_parser():
+    parser = argparse.ArgumentParser(description="Generate a report on CPAC runs.")
+    parser.add_argument(
+        "path", type=str, help="Path to the directory containing the log files."
+    )
+    parser.add_argument(
+        "-o", "--output", type=str, help="Path to the output file.", required=False
+    )
+    return parser
+
+
+def main():
+    args = make_parser().parse_args()
+    path_searchdir = pl.Path(args.path)
+    md_report = CpacRunCollection(path_searchdir, path_searchdir).report_md()
+
+    if args.output:
+        with open(args.output, "w", encoding="UTF-8") as f:
+            f.write(md_report)
+    else:
+        print(md_report)
 
 
 if __name__ == "__main__":
---
+++ src/clmunch/utils.py
@@ -0,0 +1,29 @@
+import pathlib as pl
+import re
+
+_RX_MARKDOWN_HEADING_ID_LEGAL_CHARS = re.compile(r"[^0-9a-z_-]")
+
+HTML_SYMBOL_SUCCESS = "&#9989;"  # check mark
+HTML_SYMBOL_FAILURE = "&#10060;"  # cross mark
+
+def bool_to_emoji(x: bool) -> str:
+    """Return a checkmark if x is True, a crossmark if x is False."""
+    return HTML_SYMBOL_SUCCESS if x else HTML_SYMBOL_FAILURE
+
+
+def file_tail(file: pl.Path, n: int = 10) -> str:
+    """Return the last n lines of a file."""
+    with open(file, "r", encoding="UTF-8") as f:
+        lines = f.readlines()
+        return "".join(lines[-n:])
+    
+
+def _markdown_heading_to_id(heading: str) -> str:
+    """Convert a markdown heading to a valid id to link to via (my link)[#id]."""
+    return _RX_MARKDOWN_HEADING_ID_LEGAL_CHARS.sub("", heading.lower())
+
+
+def markdown_heading_to_link(heading: str, title: str | None = None) -> str:
+    """Convert a markdown heading to a link."""
+    title = heading if title is None else title
+    return f"[{title}](#{_markdown_heading_to_id(heading)})"

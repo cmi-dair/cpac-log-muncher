626dee0f5b1c6d3c96a1309b76e64440eae10e79
Florian Rupprecht <floruppr@gmail.com>
2023-12-19 13:19:06 +0100

    Auto-simplify titles

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,2 +1,17 @@
+import clmunch.utils
+
+
 def test_dummy() -> None:
     assert True
+
+
+def test_unique_substrings() -> None:
+    assert clmunch.utils.unique_substrings(["a", "b", "c"]) == ["a", "b", "c"]
+    assert clmunch.utils.unique_substrings(["a", "b", "ab"]) == ["a", "b", "ab"]
+    assert clmunch.utils.unique_substrings(["a", "b", "ba"]) == ["a", "b", "ba"]
+    assert clmunch.utils.unique_substrings(["a", "b", "ba", "ab"]) == ["a", "b", "ba", "ab"]
+
+    assert clmunch.utils.unique_substrings(["aa", "ba"]) == ["a", "b"]
+    assert clmunch.utils.unique_substrings(["a", "aa"]) == ["a", "aa"]
+    assert clmunch.utils.unique_substrings(["aa", "a"]) == ["aa", "a"]
+    assert clmunch.utils.unique_substrings(["a123", "b123", "c123"]) == ["a", "b", "c"]
